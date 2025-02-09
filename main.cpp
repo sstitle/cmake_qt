206ac033acf5789810817616d5568cfb2b82c95b
Sam Title <samstitle@gmail.com>
2025-02-09 15:33:20 -0500

    Better but upside down

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions {
     QOpenGLShaderProgram *program;
-    GLuint vbo;
+    GLuint vbo, ebo;
     GLuint textures[2];
     float fadeFactor;
     QTimer timer;
@@ -24,6 +24,7 @@ public:
     ~MyGLWidget() {
         makeCurrent();
         glDeleteBuffers(1, &vbo);
+        glDeleteBuffers(1, &ebo);
         glDeleteTextures(2, textures);
         delete program;
         doneCurrent();
@@ -71,17 +72,22 @@ protected:
             qWarning() << "Shader program linking failed:" << program->log();
         }
 
-        // Vertex data
-        GLfloat vertices[] = {
-            // Positions
-            0.0f,  1.0f,  // First vertex
-           -1.0f, -1.0f,  // Second vertex
-            1.0f, -1.0f   // Third vertex
+        // Vertex data for the four corners of the screen
+        static const GLfloat g_vertex_buffer_data[] = { 
+            -1.0f, -1.0f,
+             1.0f, -1.0f,
+            -1.0f,  1.0f,
+             1.0f,  1.0f
         };
+        static const GLushort g_element_buffer_data[] = { 0, 1, 2, 3 };
 
         glGenBuffers(1, &vbo);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+        glGenBuffers(1, &ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_element_buffer_data), g_element_buffer_data, GL_STATIC_DRAW);
 
         // Create and load textures
         glGenTextures(2, textures);
@@ -99,6 +105,7 @@ protected:
         program->bind();
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
         int posLocation = program->attributeLocation("position");
         program->enableAttributeArray(posLocation);
         glVertexAttribPointer(posLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), nullptr);
@@ -111,7 +118,7 @@ protected:
         program->setUniformValue("textures[0]", 0);
         program->setUniformValue("textures[1]", 1);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, nullptr);
 
         program->disableAttributeArray(posLocation);
         program->release();
