46dfd4f530af5322c89f54afb4d84f22be88e496
Sam Title <samstitle@gmail.com>
2025-02-09 22:27:06 -0500

    Default shader behavior

--- cube_gl/main.cpp
+++ cube_gl/main.cpp
@@ -35,15 +35,18 @@ protected:
             #version 120
             uniform float iTime;
             uniform vec2 iResolution;
-            void main() {
+            void mainImage(out vec4 fragColor, in vec2 fragCoord) {
                 // Normalized pixel coordinates (from 0 to 1)
-                vec2 uv = gl_FragCoord.xy / iResolution.xy;
+                vec2 uv = fragCoord / iResolution.xy;
 
                 // Time varying pixel color
                 vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
 
                 // Output to screen
-                gl_FragColor = vec4(col, 1.0);
+                fragColor = vec4(col, 1.0);
+            }
+            void main() {
+                mainImage(gl_FragColor, gl_FragCoord.xy);
             }
         )";
 
